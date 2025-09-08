2a834ae2146a13596a380c1b36db6096c9518acf
Faisalkarim <51298879+Faisalkarim@users.noreply.github.com>
2025-09-08 22:16:35 +0000

    Deploying to gh-pages from @ Faisalkarim/karimfaisal@6ff0ef8ee30135e32d0bcc307c2da57e8241f318 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/karimfaisal/cv/";
+            window.location.href = "/karimfaisal/https:/drive.google.com/file/d/1okVnaXh10gMfZPv2ytv65kP0eW0gMzyJ/view?usp=sharing";
           },
         },{id: "post-a-post-with-plotly-js",
         
